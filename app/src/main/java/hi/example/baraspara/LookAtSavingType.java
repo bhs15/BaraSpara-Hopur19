2c9c5285d770a696fb5b546c0807e51fcbb28ea9
THWinther <43626565+THWinther@users.noreply.github.com>
2021-03-11 01:02:50 +0000

    smella a savingtype fær núna á transactions þarf núna að sýna listan

--- app/src/main/java/hi/example/baraspara/LookAtSavingType.java
+++ app/src/main/java/hi/example/baraspara/LookAtSavingType.java
@@ -7,7 +7,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class LookAtSavingType extends AppCompatActivity {
+
+    String id;
+    String transactions;
+
+    class GetThread implements Runnable{
+        private volatile String data;
+        @Override
+        public void run() {
+            try {
+                this.data = Get("http://10.0.2.2:8080/lookAtTransactions/"+id);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public String getData(){return this.data;}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +44,7 @@ public class LookAtSavingType extends AppCompatActivity {
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("Name");
-        String id = intent.getStringExtra("Id");
+        id = intent.getStringExtra("Id");
         String amount = intent.getStringExtra("Amount");
 
         TextView savingTypeName = (TextView) findViewById(R.id.savingTypeName);
@@ -27,7 +54,27 @@ public class LookAtSavingType extends AppCompatActivity {
         savingTypeAmount.setText(amount);
 
         RecyclerView transactionViewer = (RecyclerView) findViewById(R.id.transactionViewer);
+        
+        try {
+        GetThread gt = new GetThread();
+        Thread get = new Thread(gt);
+        get.start();
+        get.join();
+        transactions=gt.getData();
+        System.out.println(transactions);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    String Get(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
     }
 }
